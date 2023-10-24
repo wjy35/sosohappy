5d663a7f695027f575c9c597a4f497e4b2091aa5
seokJKIM <tjrwn1247@naver.com>
2023-10-24 21:17:58 +0900

    :hammer: [BE] Refactor: HelpHistoryRequest 수정

---
+++ back/help-history-query/src/main/java/com/ssafy/helphistoryquery/api/request/HelpHistoryRequest.java
@@ -0,0 +1,25 @@
+package com.ssafy.helphistoryquery.api.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@Getter
+@Builder
+public class HelpHistoryRequest {
+    private long historyId;
+    private long toMemberId;
+    private long fromMemberId;
+    private long categoryId;
+    private String content;
+    private BigDecimal x;
+    private BigDecimal y;
+    private Timestamp createdAt;
+}
+
+
+
