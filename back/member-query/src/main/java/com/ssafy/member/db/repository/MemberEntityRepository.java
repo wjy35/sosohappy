b296851d0f9d4b34832bf6d73b6b8087107aedf5
왕준영 <wangsun7@naver.com>
2023-10-24 21:35:01 +0900

    :heavy_plus_sign: [BE] Feat: MemberEntityRepository 에 existsByNickname interface 추가

--- back/member-query/src/main/java/com/ssafy/member/db/repository/MemberEntityRepository.java
+++ back/member-query/src/main/java/com/ssafy/member/db/repository/MemberEntityRepository.java
@@ -4,4 +4,5 @@ import com.ssafy.member.db.entity.MemberEntity;
 
 public interface MemberEntityRepository {
     MemberEntity findByMemberId(Long memberId);
+    Boolean existsByNickname(String nickname);
 }
