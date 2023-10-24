5e99568d4c3f1b498da3819b7526d20ac5637cc4
seokJKIM <tjrwn1247@naver.com>
2023-10-24 21:16:44 +0900

    :construction_worker: [BE] Setting: RedisConfig 변경

--- back/help-history-query/src/main/java/com/ssafy/helphistoryquery/config/RedisConfig.java
+++ back/help-history-query/src/main/java/com/ssafy/helphistoryquery/config/RedisConfig.java
@@ -1,14 +1,18 @@
 package com.ssafy.helphistoryquery.config;
 
+import com.ssafy.helphistoryquery.api.request.HelpHistoryRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
@@ -17,18 +21,25 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int port;
 
+//    @Value("${spring.redis.password}")
+//    private int password;
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    public RedisTemplate<String, HelpHistoryRequest> redisTemplate() {
+        RedisTemplate<String, HelpHistoryRequest> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer()); // Use JSON serialization for values
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
     }
 
+    @Bean
+    public ListOperations<String, HelpHistoryRequest> listOperations(){
+        return this.redisTemplate().opsForList();
+    }
 }
