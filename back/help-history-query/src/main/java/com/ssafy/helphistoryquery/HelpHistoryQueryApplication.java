c0fa91907fdfda8588d2d6bedca5b6970cd3d24c
seokJKIM <tjrwn1247@naver.com>
2023-10-24 13:37:20 +0900

    :construction_worker: [BE] Setting: init

---
+++ back/help-history-query/src/main/java/com/ssafy/helphistoryquery/HelpHistoryQueryApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.helphistoryquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpHistoryQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpHistoryQueryApplication.class, args);
+	}
+
+}
