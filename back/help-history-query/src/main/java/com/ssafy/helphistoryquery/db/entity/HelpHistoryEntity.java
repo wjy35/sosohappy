23d6157c0368a4c78901127518a1a2b6fdd7de2f
seokJKIM <tjrwn1247@naver.com>
2023-10-25 14:09:41 +0900

    :hammer: [BE] Refactor: HelpHistoryEntity 타입 수정

--- back/help-history-query/src/main/java/com/ssafy/helphistoryquery/db/entity/HelpHistoryEntity.java
+++ back/help-history-query/src/main/java/com/ssafy/helphistoryquery/db/entity/HelpHistoryEntity.java
@@ -2,20 +2,35 @@ package com.ssafy.helphistoryquery.db.entity;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
-import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 
 @Getter
-@Builder
+@ToString
+@RequiredArgsConstructor
 public class HelpHistoryEntity {
     private long historyId;
     private long toMemberId;
     private long fromMemberId;
     private long categoryId;
     private String content;
-    private BigDecimal x;
-    private BigDecimal y;
+    private double x;
+    private double y;
     private Timestamp createdAt;
+
+    @Builder
+    public HelpHistoryEntity(long historyId, long toMemberId, long fromMemberId, long categoryId, String content, double x, double y, Timestamp createdAt) {
+        this.historyId = historyId;
+        this.toMemberId = toMemberId;
+        this.fromMemberId = fromMemberId;
+        this.categoryId = categoryId;
+        this.content = content;
+        this.x = x;
+        this.y = y;
+        this.createdAt = createdAt;
+    }
 }
+
