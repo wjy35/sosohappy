cbe37e9b14543cd10068c552554414d8477408d3
왕준영 <wangsun7@naver.com>
2023-10-25 09:25:59 +0900

    :heavy_plus_sign: [BE] Feat: 닉네임 중복 검사 API 추가

--- back/member-query/src/main/java/com/ssafy/member/api/controller/Controller.java
+++ back/member-query/src/main/java/com/ssafy/member/api/controller/Controller.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,5 +38,21 @@ public class Controller {
 
         return new ResponseEntity<>(formattedResponse,HttpStatus.OK);
     }
+
+    @GetMapping("/availability/nickname/{nickname}")
+    ResponseEntity<FormattedResponse> isNicknameAvailable(@PathVariable String nickname){
+        Boolean availability = Optional.of(memberInformationService.isNicknameAvailable(nickname)).get();
+
+        FormattedResponse formattedResponse = FormattedResponse
+                .builder()
+                .status("success")
+                .message("SUCCESS NICKNAME DUPLICATE CHECK")
+                .result("availability",availability)
+                .build();
+
+        return new ResponseEntity<>(formattedResponse,HttpStatus.OK);
+    }
+
+
 
 }
