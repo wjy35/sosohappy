20660cd83c9201b6d720674aa6f14a65c249db25
왕준영 <wangsun7@naver.com>
2023-10-26 13:34:26 +0900

    :heavy_plus_sign: [BE] Feat: Jwt관련 Util 추가

---
+++ back/member-command/src/main/java/com/ssafy/member/util/JwtUtil.java
@@ -0,0 +1,51 @@
+package com.ssafy.member.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final Long EXPIRE_MIN;
+    private final Key key;
+
+    @Autowired
+    public JwtUtil(@Value("JWT_SALT")String salt,@Value("JWT_EXPIRE_MIN")Long expireMin) {
+        this.EXPIRE_MIN = expireMin;
+        byte[] keyBytes = Base64.getDecoder().decode(Base64.getEncoder().encode(salt.getBytes()));
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private String generateToken(Long memberId,Long expireMin){
+        return Jwts.builder()
+                .claim("memberId", memberId)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .setExpiration(new Date(System.currentTimeMillis()+expireMin))
+                .compact();
+    }
+
+    public String generateAccessToken(Long memberId){
+        return generateToken(memberId,this.EXPIRE_MIN);
+    }
+
+    public String generateRefreshToken(Long memberId){
+        return generateToken(memberId,this.EXPIRE_MIN);
+    }
+
+    public Long getMemberIdByToken(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .get("memberId",Long.class);
+    }
+
+}
+
