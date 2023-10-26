66cb1e11690dd34a336eae962438f82f8d1df70b
왕준영 <wangsun7@naver.com>
2023-10-26 14:52:36 +0900

    :heavy_plus_sign: [BE] Feat: memberSignId로 Member Entity를 조회하는 기능 추가

--- back/member-command/src/main/java/com/ssafy/member/db/repository/MemberEntityRepository.java
+++ back/member-command/src/main/java/com/ssafy/member/db/repository/MemberEntityRepository.java
@@ -3,5 +3,8 @@ package com.ssafy.member.db.repository;
 import com.ssafy.member.db.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberEntityRepository extends JpaRepository<MemberEntity,Long> {
+    Optional<MemberEntity> findByMemberSignId(String memberSignId);
 }
