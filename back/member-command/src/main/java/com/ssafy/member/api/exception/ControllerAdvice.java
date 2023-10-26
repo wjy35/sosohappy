8f6befc595634e443fa1581c21d544c00e4af6b2
왕준영 <wangsun7@naver.com>
2023-10-26 17:41:30 +0900

    :hammer: [BE] Refactor: CustomException Handler log format 수정

--- back/member-command/src/main/java/com/ssafy/member/api/exception/ControllerAdvice.java
+++ back/member-command/src/main/java/com/ssafy/member/api/exception/ControllerAdvice.java
@@ -1,19 +1,23 @@
 package com.ssafy.member.api.exception;
 
 import com.ssafy.member.api.response.FormattedResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class ControllerAdvice {
 
     @ExceptionHandler(CustomException.class)
-    public FormattedResponse handleCustomException(CustomException e) {
-        return FormattedResponse
-                .builder()
+    public FormattedResponse onCustomException(CustomException customException) {
+        log.debug("{}",customException.getMessage());
+
+        return FormattedResponse.builder()
                 .status("fail")
-                .message(e.getErrorCode().getMessage())
+                .message(customException.getErrorCode().getMessage())
                 .build();
     }
+
 
 }
--- back/member-command/src/main/java/com/ssafy/member/api/exception/CustomException.java
+++ back/member-command/src/main/java/com/ssafy/member/api/exception/CustomException.java
@@ -1,13 +1,17 @@
 package com.ssafy.member.api.exception;
 
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
+@Slf4j
 public class CustomException extends RuntimeException {
 
     private ErrorCode errorCode;
     public CustomException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.setStackTrace(new StackTraceElement[0]);
         this.errorCode = errorCode;
     }
+
 }
