c94b4b56e220abdf12dcac7319057cd99a07ea7a
왕준영 <wangsun7@naver.com>
2023-10-27 10:28:19 +0900

    :white_check_mark: [BE] Test: Token 등록 용 임시 코드 추가

--- back/notification/src/main/java/com/ssafy/notification/api/controller/Controller.java
+++ back/notification/src/main/java/com/ssafy/notification/api/controller/Controller.java
@@ -1,5 +1,9 @@
 package com.ssafy.notification.api.controller;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 import com.ssafy.notification.api.mapper.MemberDeviceMapper;
 import com.ssafy.notification.api.request.MemberDeviceSaveRequest;
 import com.ssafy.notification.api.response.FormattedResponse;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class Controller {
     private final MemberDeviceManageService memberDeviceManageService;
+    private final FirebaseMessaging firebaseMessaging;
 
     @PostMapping("/")
     ResponseEntity<FormattedResponse> save(@RequestBody MemberDeviceSaveRequest memberDeviceSaveRequest){
@@ -25,6 +30,24 @@ public class Controller {
                 .status("success")
                 .message("SUCCESS SAVE")
                 .build();
+
+        // Todo Test 용 임시 코드 삭제
+        Notification notification = Notification
+                .builder()
+                .setTitle("소소행")
+                .setBody("응애 나 애기 석주")
+                .build();
+
+        Message message = Message
+                .builder()
+                .setToken(memberDeviceSaveRequest.getDeviceToken())
+                .setNotification(notification)
+                .build();
+        try {
+            firebaseMessaging.send(message);
+        } catch (FirebaseMessagingException e) {
+            throw new RuntimeException(e);
+        }
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
