b1680ab0fd92bf77952f4a4991b0ed1661a08c83
youn <yyytae0@gmail.com>
2023-10-27 10:42:10 +0900

    :heavy_plus_sign: [FE] Feat: add event when appstate switch

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navigation from '@/navigators/Navigation';
+import useLocation from "@/hooks/useLocation";
+import {AppState} from "react-native";
 
 function App(): JSX.Element {
+    const location = useLocation({});
+    useEffect(() => {
+        console.log('gdgd')
+        const appState = AppState.addEventListener('change', ()=>{
+            if (AppState.currentState === 'active'){
+                location.setForeground();
+            } else if (AppState.currentState === 'background'){
+                location.setBackground();
+            }
+        });
+        return () => {
+            appState.remove();
+        }
+    }, []);
 
-  return (
+    return (
     <>
       <Navigation/>
     </>
-  );
+    );
 }
 export default App;
