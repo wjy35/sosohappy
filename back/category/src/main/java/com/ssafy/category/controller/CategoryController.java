f5ec934a6dd9486d26ffa6cdd8697b54ba98e642
정민희 <mong2072@naver.com>
2023-10-27 13:42:58 +0900

    :heavy_plus_sign: [BE] Feat: 카테고리 리스트 반환 api 추가

--- back/category/src/main/java/com/ssafy/category/controller/CategoryController.java
+++ back/category/src/main/java/com/ssafy/category/controller/CategoryController.java
@@ -1,32 +1,43 @@
 package com.ssafy.category.controller;
 
 import com.ssafy.category.common.response.FormattedResponse;
-import com.ssafy.category.domain.entity.Category;
+import com.ssafy.category.domain.res.CategoryRes;
 import com.ssafy.category.service.CategoryServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class CategoryController {
     private final CategoryServiceImpl categoryServiceImpl;
 
-    @Autowired
-    public CategoryController(CategoryServiceImpl categoryServiceImpl) {
-        this.categoryServiceImpl = categoryServiceImpl;
-    }
-
     @GetMapping("/getCategoryInfo/{categoryId}")
     public ResponseEntity<FormattedResponse> getCategoryInfo(@PathVariable Long categoryId) {
-        Category category = categoryServiceImpl.getCategoryInfo(categoryId);
+        CategoryRes categoryRes = categoryServiceImpl.getCategoryInfo(categoryId);
 
         FormattedResponse formattedResponse = FormattedResponse.builder()
                 .status("success")
                 .message("카테고리 정보를 성공적으로 불러왔습니다.")
-                .result("category",category)
+                .result("category",categoryRes)
+                .build();
+
+        return new ResponseEntity<>(formattedResponse, HttpStatus.OK);
+    }
+
+    @GetMapping("/categories")
+    public ResponseEntity<FormattedResponse> getdefaultCategoryList() {
+        List<CategoryRes> categoryResList = categoryServiceImpl.getdefaultCategoryList();
+
+        FormattedResponse formattedResponse = FormattedResponse.builder()
+                .status("success")
+                .message("카테고리 정보를 성공적으로 불러왔습니다.")
+                .result("defaultCategoryList",categoryResList)
                 .build();
 
         return new ResponseEntity<>(formattedResponse, HttpStatus.OK);
