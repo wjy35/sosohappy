5536cb189702354d1f2835b37667be54c984399e
왕준영 <wangsun7@naver.com>
2023-10-27 10:26:33 +0900

    :white_check_mark: [BE] Test: Test Notification 기능 추가

--- back/notification/src/test/java/com/ssafy/notification/NotificationApplicationTests.java
+++ back/notification/src/test/java/com/ssafy/notification/NotificationApplicationTests.java
@@ -1,13 +1,51 @@
 package com.ssafy.notification;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import com.ssafy.notification.db.entity.MemberDeviceEntity;
+import com.ssafy.notification.db.repository.MemberDeviceEntityRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class NotificationApplicationTests {
+
+    @Autowired
+    FirebaseMessaging firebaseMessaging;
+
+    @Autowired
+    MemberDeviceEntityRepository memberDeviceEntityRepository;
 
     @Test
     void contextLoads() {
+
+    }
+
+    @Test
+    void testNotification(){
+        Notification notification = Notification
+                .builder()
+                .setTitle("소소행")
+                .setBody("응애 나 애기 석주")
+                .build();
+
+        Message message = Message
+                .builder()
+                .setToken(memberDeviceEntityRepository.findByMemberId(1l).getDeviceToken())
+                .setNotification(notification)
+                .build();
+
+        try {
+            firebaseMessaging.send(message);
+        } catch (FirebaseMessagingException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
