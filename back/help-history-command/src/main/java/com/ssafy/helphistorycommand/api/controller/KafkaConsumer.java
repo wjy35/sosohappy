90bae33e79bd685d160f0d6fb39faf8e3ebe178d
seokJKIM <tjrwn1247@naver.com>
2023-10-30 18:05:30 +0900

    :hammer: [BE] Refactor: KafkaConsumer consume 수정

--- back/help-history-command/src/main/java/com/ssafy/helphistorycommand/api/controller/KafkaConsumer.java
+++ back/help-history-command/src/main/java/com/ssafy/helphistorycommand/api/controller/KafkaConsumer.java
@@ -1,15 +1,22 @@
 package com.ssafy.helphistorycommand.api.controller;
 
 
+import com.ssafy.helphistorycommand.api.request.HelpHistoryRequest;
+import com.ssafy.helphistorycommand.service.HelpHistoryService;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Controller;
 
 
 @Controller
+@RequiredArgsConstructor
 public class KafkaConsumer {
 
+    private final HelpHistoryService helpHistoryService;
     @KafkaListener(topics = "help-history-command-mysql.help_history.help_history")
-    public void consume(String message) {
-        System.out.println("message = " + message);
+    public void consume(ConsumerRecord<String,String> message) {
+//        helpHistoryService.addHelpHistory(message);
+        System.out.println("message = " + message.value());
     }
 }
