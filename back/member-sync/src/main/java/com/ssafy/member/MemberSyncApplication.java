a036a7c21d1f3811aedf9b7e583cd15e743b5077
왕준영 <wangsun7@naver.com>
2023-10-30 13:29:10 +0900

    :construction_worker: [BE] Setting: init

---
+++ back/member-sync/src/main/java/com/ssafy/member/MemberSyncApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberSyncApplication.class, args);
+    }
+
+}
