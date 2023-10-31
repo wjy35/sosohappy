fceb5cce7c3c34819128f60549b074957c404249
윤태영 <rhkwpdusrn22@naver.com>
2023-10-31 19:07:05 +0900

    :heavy_plus_sign: [FE] Feat: test code for sosomon dictionary

--- front/src/components/SosomonDictionary.tsx
+++ front/src/components/SosomonDictionary.tsx
@@ -1,45 +1,35 @@
 import React from 'react';
 import {View} from "react-native";
 import SingleSosomon from "@/components/SingleSosomon";
-import {type1} from '@/assets/sosomon/index'
-// import test from "@/assets/sosomon/type1/FennecFox.png";
-import FennecFox from "@/assets/sosomon/type1/FennecFox.png";
-import Buffalo from "@/assets/sosomon/type1/Buffalo.png";
-import Dog from "@/assets/sosomon/type1/Dog.png";
-import Elephant from "@/assets/sosomon/type1/Elephant.png";
-import Fox from "@/assets/sosomon/type1/Fox.png";
-import Gazelle from "@/assets/sosomon/type1/Gazelle.png";
-import Ox from "@/assets/sosomon/type1/Ox.png";
-import Pig from "@/assets/sosomon/type1/Pig.png";
-import Rabbit from "@/assets/sosomon/type1/Rabbit.png";
-import Zebra from "@/assets/sosomon/type1/Zebra.png";
-
+import {type1, type2, type3, type4} from '@/assets/sosomon/index'
 
 const SosomonDictionary = ({}) => {
 
     return (
         <View style={[{borderWidth: 1}]}>
             <View style={[{flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'center'}]}>
-                {/*{*/}
-                {/*    type1.map((el, idx)=>{*/}
-                {/*        console.log(el);*/}
-                {/*        return (*/}
-                {/*            // <React.Fragment }>*/}
-                {/*                <SingleSosomon src={el} key={`type1-${idx}`} />*/}
-                {/*            // </React.Fragment>*/}
-                {/*        )*/}
-                {/*    })*/}
-                {/*}*/}
-                <SingleSosomon src={FennecFox}/>
-                <SingleSosomon src={Buffalo}/>
-                <SingleSosomon src={Dog}/>
-                <SingleSosomon src={Elephant}/>
-                <SingleSosomon src={Fox}/>
-                <SingleSosomon src={Gazelle}/>
-                <SingleSosomon src={Ox}/>
-                <SingleSosomon src={Pig}/>
-                <SingleSosomon src={Rabbit}/>
-                <SingleSosomon src={Zebra}/>
+                {
+                    type1.map((el, idx)=>{
+                        return (
+                                <SingleSosomon src={el} key={`type1-${idx}`} />
+                        )
+                    })
+                }
+                {
+                    type2.map((el, idx)=>{
+                        return (
+                          <SingleSosomon src={el} key={`type2-${idx}`} />
+                        )
+                    })
+                }
+                {
+                    type3.map((el, idx)=>{
+                        return (
+                          <SingleSosomon src={el} key={`type3-${idx}`} />
+                        )
+                    })
+                }
+                <SingleSosomon src={type4[0]}/>
             </View>
         </View>
     )
