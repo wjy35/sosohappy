fdbe13b90210c1255de5f26f79089516267c3153
왕준영 <wangsun7@naver.com>
2023-10-31 17:09:00 +0900

    :heavy_plus_sign: [BE] Feat: Kafka Consumer를 통해 DB와 Redis 를 동기화 하는 기능 추가

--- back/member-sync/src/main/java/com/ssafy/member/consumer/KafkaConsumer.java
+++ back/member-sync/src/main/java/com/ssafy/member/consumer/KafkaConsumer.java
@@ -2,25 +2,26 @@ package com.ssafy.member.consumer;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ssafy.member.consumer.event.MemberEvent;
+import com.ssafy.member.consumer.event.MemberEventWorker;
 import com.ssafy.member.util.KafkaEventMapper;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class KafkaConsumer {
     private final KafkaEventMapper kafkaEventMapper;
+    private final MemberEventWorker memberEventWorker;
 
     @KafkaListener(topics = "member-command-mysql.member.member")
     public void consume(ConsumerRecord<?,?> consumerRecord) throws JsonProcessingException {
         if(isEmptyEvent(consumerRecord)) return;
 
         MemberEvent memberEvent = kafkaEventMapper.toEvent(consumerRecord, MemberEvent.class);
-        System.out.println("memberEvent = " + memberEvent);
+        memberEventWorker.work(memberEvent);
     }
 
     private boolean isEmptyEvent(ConsumerRecord<?,?> consumerRecord){
