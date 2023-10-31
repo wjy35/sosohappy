38850bc9091b7502659d63e9e0ec159d161fd236
seokJKIM <tjrwn1247@naver.com>
2023-10-31 16:33:37 +0900

    :heavy_plus_sign: [BE] Feat: addHelpHistory 기능 추가

---
+++ back/help-history-sync/src/main/java/com/ssafy/helphistorysync/api/contorller/KafkaConsumer.java
@@ -0,0 +1,21 @@
+package com.ssafy.helphistorysync.api.contorller;
+
+import com.ssafy.helphistorysync.api.service.HelpHistoryService;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class KafkaConsumer {
+
+    private final HelpHistoryService helpHistoryService;
+
+    @KafkaListener(topics = "help-history-command-mysql.help_history.help_history")
+    public void consume(ConsumerRecord<String,String> message) {
+        helpHistoryService.addHelpHistory(message);
+        System.out.println("message = " + message.value());
+    }
+}
+
