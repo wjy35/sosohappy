ce676cbaee8c222211c2196363f02ce7427ac859
seokJKIM <tjrwn1247@naver.com>
2023-10-31 13:51:39 +0900

    :heavy_plus_sign: [BE] Feat: addHelpHistory

--- back/help-history-command/src/main/java/com/ssafy/helphistorycommand/api/controller/KafkaConsumer.java
+++ back/help-history-command/src/main/java/com/ssafy/helphistorycommand/api/controller/KafkaConsumer.java
@@ -1,8 +1,7 @@
 package com.ssafy.helphistorycommand.api.controller;
 
 
-import com.ssafy.helphistorycommand.api.request.HelpHistoryRequest;
-import com.ssafy.helphistorycommand.service.HelpHistoryService;
+import com.ssafy.helphistorycommand.api.service.HelpHistoryService;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -16,7 +15,7 @@ public class KafkaConsumer {
     private final HelpHistoryService helpHistoryService;
     @KafkaListener(topics = "help-history-command-mysql.help_history.help_history")
     public void consume(ConsumerRecord<String,String> message) {
-//        helpHistoryService.addHelpHistory(message);
+        helpHistoryService.addHelpHistory(message);
         System.out.println("message = " + message.value());
     }
 }
