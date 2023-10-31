97b4b2ab6f1eaf081abe63d8dde15452ccd3b346
livinoid98 <ccy9803@gmail.com>
2023-10-31 14:24:32 +0900

    :heavy_plus_sign: [FE] Feat(map-route-line): 맵 페이지 출발지 ~ 도착지 경로 그리기
    - tmap api 도보 경로 찾기 api 연동
    - 출발지 ~ 도착지까지 point 및 lineString 배열로 반환
    - 위 배열을 가지고 render 시 PolyLine으로 거점지 이어주기
    - .env 설정으로 api key 분리
    - todo. 현재 정적으로 멀캠~파이낸셜센터까지 경로를 받아옴 -> 추후 등록자와 내 GPS 정보로 동적으로 변동

--- front/babel.config.js
+++ front/babel.config.js
@@ -21,5 +21,14 @@ module.exports = {
         },
       },
     ],
+    [
+      "module:react-native-dotenv",
+      {
+        moduleName: "@env",
+        path: ".env",
+        safe: false,
+        aloowUndefined: true,
+      },
+    ],
   ],
 };
---
+++ front/env.d.ts
@@ -0,0 +1,3 @@
+declare module "@env" {
+    export const MAP_LINE_API_KEY: string;
+}
