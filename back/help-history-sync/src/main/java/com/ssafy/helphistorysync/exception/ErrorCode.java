9dc3ebf08ffceb8ba0f3d1606bca40473981dcfb
seokJKIM <tjrwn1247@naver.com>
2023-10-31 17:31:21 +0900

    :hammer: [BE] Refactor: ErrorCode 수정

--- back/help-history-sync/src/main/java/com/ssafy/helphistorysync/exception/ErrorCode.java
+++ back/help-history-sync/src/main/java/com/ssafy/helphistorysync/exception/ErrorCode.java
@@ -8,8 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    CLASS_CAST_ERROR(HttpStatus.BAD_REQUEST, "직렬화 되지 않은 데이터 삽입 실패"),
-    SERIALIZATION_ERROR_MESSAGE(HttpStatus.BAD_REQUEST, "직렬화에 실패했습니다.");
+    CLASS_CAST_ERROR(HttpStatus.BAD_REQUEST, "직렬화에 실패했습니다.");
 
     private final HttpStatus status;
     private final String message;
