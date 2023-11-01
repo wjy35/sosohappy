f030ccc05b5884dcc466929a97d03c7e73bbeae1
seokJKIM <tjrwn1247@naver.com>
2023-11-01 15:42:56 +0900

    :heavy_plus_sign: [BE] Feat: add CategoryFeign 카테고리 정보 조회

--- back/help-history-sync/src/main/java/com/ssafy/helphistorysync/HelpHistorySyncApplication.java
+++ back/help-history-sync/src/main/java/com/ssafy/helphistorysync/HelpHistorySyncApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class HelpHistorySyncApplication {
 
