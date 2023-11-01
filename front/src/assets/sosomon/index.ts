3ad336a807034fca11bb172ef8fede4982397ac7
livinoid98 <ccy9803@gmail.com>
2023-11-01 13:00:01 +0900

    :art: [FE] Design(character-modal-publish): 캐릭터 도감 모달 페이지 퍼블리싱 완료
    - 캐릭터 도감 모달 페이지 퍼블리싱 완료
    - sosomoncard 컴포넌트 구성 완료

--- front/src/assets/sosomon/index.ts
+++ front/src/assets/sosomon/index.ts
@@ -1,15 +1,3 @@
-import FennecFox from "@/assets/sosomon/type1/FennecFox.png";
-import Buffalo from "@/assets/sosomon/type1/Buffalo.png";
-import Dog from "@/assets/sosomon/type1/Dog.png";
-import Elephant from "@/assets/sosomon/type1/Elephant.png";
-import Fox from "@/assets/sosomon/type1/Fox.png";
-import Gazelle from "@/assets/sosomon/type1/Gazelle.png";
-import Ox from "@/assets/sosomon/type1/Ox.png";
-import Pig from "@/assets/sosomon/type1/Pig.png";
-import Rabbit from "@/assets/sosomon/type1/Rabbit.png";
-import Zebra from "@/assets/sosomon/type1/Zebra.png";
-
-
 export const type1 = [
     require('./type1/FennecFox.png'),
     require('./type1/Rabbit.png'),
