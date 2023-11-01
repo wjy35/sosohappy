310bc61b0391ac4be67f3deae5cd8aafbea40de5
seokJKIM <tjrwn1247@naver.com>
2023-11-01 15:48:02 +0900

    :heavy_plus_sign: [BE] Feat: Sync 데이터 category 정보 추가

--- back/help-history-sync/src/main/java/com/ssafy/helphistorysync/api/service/impl/HelpHistoryServiceImpl.java
+++ back/help-history-sync/src/main/java/com/ssafy/helphistorysync/api/service/impl/HelpHistoryServiceImpl.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.helphistorysync.api.mapper.HelpHistoryMapper;
+import com.ssafy.helphistorysync.api.request.CategoryRequest;
 import com.ssafy.helphistorysync.api.request.HelpHistoryRequest;
 import com.ssafy.helphistorysync.api.service.HelpHistoryService;
+import com.ssafy.helphistorysync.cloud.feign.CategoryFeign;
 import com.ssafy.helphistorysync.db.entity.HelpHistoryEntity;
 import com.ssafy.helphistorysync.db.repository.HelpHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -22,12 +25,16 @@ public class HelpHistoryServiceImpl implements HelpHistoryService {
 
     private final HelpHistoryRepository helpHistoryRepository;
 
+    private final CategoryFeign categoryFeign;
+
     @Override
     public void addHelpHistory(ConsumerRecord<String, String> message) throws JsonProcessingException {
 
         HelpHistoryRequest helpHistoryRequest = getAfter(message);
 
-        HelpHistoryEntity helpHistoryEntity = helpHistoryMapper.requestToEntity(helpHistoryRequest);
+        CategoryRequest categoryRequest = getCategory(helpHistoryRequest);
+
+        HelpHistoryEntity helpHistoryEntity = helpHistoryMapper.requestToEntity(helpHistoryRequest, categoryRequest);
 
         helpHistoryRepository.addHelpHistory(helpHistoryEntity);
     }
@@ -37,4 +44,22 @@ public class HelpHistoryServiceImpl implements HelpHistoryService {
         JsonNode jsonNode = objectMapper.readTree(message.value());
         return objectMapper.treeToValue(jsonNode.get("after"), HelpHistoryRequest.class);
     }
+
+    @Override
+    public CategoryRequest getCategory(HelpHistoryRequest helpHistoryRequest) throws JsonProcessingException {
+
+        String jsonString = categoryFeign.getCategoryDetail(helpHistoryRequest.getCategoryId());
+
+        JSONObject jsonObject = new JSONObject(jsonString).getJSONObject("result").getJSONObject("category");
+
+        long categoryId = jsonObject.getLong("categoryId");
+        String categoryName = jsonObject.getString("categoryName");
+        String categoryImage = jsonObject.getString("categoryImage");
+
+        return CategoryRequest.builder()
+                .categoryId(categoryId)
+                .categoryName(categoryName)
+                .categoryImage(categoryImage).build();
+    }
+
 }
