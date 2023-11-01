4994bd3b498e95c2bfa8ebefa6e24c028ca6e5f1
youn <yyytae0@gmail.com>
2023-11-02 08:49:40 +0900

    :heavy_plus_sign: [FE] Feat: check permission to devide show menu

--- front/src/components/SideMenu.tsx
+++ front/src/components/SideMenu.tsx
@@ -1,23 +1,35 @@
-import {View, Text, Image, TouchableOpacity} from "react-native"
-import CloseIcon from "@/assets/img/close-icon.png"
+import {View, Text, TouchableOpacity} from "react-native"
 import SideMenuStyle from "@/styles/SideMenuStyle";
 import {useNavigation} from "@react-navigation/native";
-import { sosohappyWhiteLogo } from "@/assets/icons/icons";
+import { sosohappyWhiteLogo, user, gear, chat, peace, home, close } from "@/assets/icons/icons";
 import { SvgXml } from "react-native-svg";
-
-import HomeIcon from "@/assets/img/home-gray-icon.png"
-import HelpIcon from "@/assets/img/help-gray-icon.png"
-import ChatIcon from "@/assets/img/chat-gray-icon.png"
-import MyPageIcon from "@/assets/img/mypage-gray-icon.png"
-import SettingIcon from "@/assets/img/setting-gray-icon.png"
+import {observer} from "mobx-react";
+import useStore from "@/hooks/useStore";
+import memberApi from "@/apis/memberApi";
 
 interface props {
     closeSide: Function;
     nowPage?: string;
 }
 
-const SideMenu = ({closeSide, nowPage}: props) => {
+const SideMenu = observer(({closeSide, nowPage}: props) => {
     const navigation = useNavigation();
+    const {userStore} = useStore();
+
+    const logout = async () => {
+        try {
+            const res = await memberApi.logout();
+            if (res.status === 200){
+                userStore.setUser(null);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const goto = (next: string) => {
+        userStore.user?(navigation.navigate(next)):(navigation.navigate('Login'));
+    }
 
     return(
         <>
@@ -32,54 +44,72 @@ const SideMenu = ({closeSide, nowPage}: props) => {
                         <Text style={SideMenuStyle.logoText}>소소행</Text>
                     </View>
                     <TouchableOpacity activeOpacity={0.7} onPress={() => closeSide()}>
-                        <Image
-                            source={CloseIcon}
-                            style={SideMenuStyle.closeIcon}
+                        <SvgXml
+                            xml={close}
+                            width={16}
+                            height={16}
                         />
                     </TouchableOpacity>
                 </View>
+                {
+                    userStore.user && (
+                        <View style={SideMenuStyle.nickNameWrap}>
+                            <Text>{userStore.user.nickname}</Text>
+                        </View>
+                    )
+                }
+
                 <View style={SideMenuStyle.navWrap}>
                     <TouchableOpacity activeOpacity={0.7} onPress={()=>navigation.navigate("Main")}>
                         <View style={[SideMenuStyle.menuList, nowPage==="Main" && SideMenuStyle.menuListActive]}>
-                            <Image
-                                source={HomeIcon}
-                                style={SideMenuStyle.navIcon}
+                            <SvgXml
+                                xml={home}
+                                width={24}
+                                height={24}
                             />
                             <Text style={[SideMenuStyle.menuItemText]}>메인</Text>
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity activeOpacity={0.7} onPress={()=>navigation.navigate("CreateHelp")}>
-                        <View style={[SideMenuStyle.menuList, nowPage==="Help" && SideMenuStyle.menuListActive]}>
-                            <Image
-                                source={HelpIcon}
-                                style={SideMenuStyle.navIcon}
-                            />
-                            <Text style={[SideMenuStyle.menuItemText]}>도움 요청</Text>
-                        </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity activeOpacity={0.7} onPress={()=>navigation.navigate("ChatList")}>
+                    {
+                        userStore.user?.disabled === true && (
+                            <TouchableOpacity activeOpacity={0.7} onPress={()=>goto("CreateHelp")}>
+                                <View style={[SideMenuStyle.menuList, nowPage==="Help" && SideMenuStyle.menuListActive]}>
+                                    <SvgXml
+                                        xml={peace}
+                                        width={24}
+                                        height={24}
+                                    />
+                                    <Text style={[SideMenuStyle.menuItemText]}>도움 요청</Text>
+                                </View>
+                            </TouchableOpacity>
+                        )
+                    }
+                    <TouchableOpacity activeOpacity={0.7} onPress={()=>goto("ChatList")}>
                         <View style={[SideMenuStyle.menuList, nowPage==="Chat" && SideMenuStyle.menuListActive]}>
-                            <Image
-                                source={ChatIcon}
-                                style={SideMenuStyle.navIcon}
+                            <SvgXml
+                                xml={chat}
+                                width={24}
+                                height={24}
                             />
                             <Text style={[SideMenuStyle.menuItemText]}>채팅</Text>
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity activeOpacity={0.7} onPress={()=>navigation.navigate("MyPage")}>
+                    <TouchableOpacity activeOpacity={0.7} onPress={()=>goto("MyPage")}>
                         <View style={[SideMenuStyle.menuList, nowPage==="MyPage" && SideMenuStyle.menuListActive]}>
-                            <Image
-                                source={MyPageIcon}
-                                style={SideMenuStyle.navIcon}
+                            <SvgXml
+                                xml={user}
+                                width={24}
+                                height={24}
                             />
                             <Text style={[SideMenuStyle.menuItemText]}>마이페이지</Text>
                         </View>
                     </TouchableOpacity>
                     <TouchableOpacity activeOpacity={0.7}>
                         <View style={[SideMenuStyle.menuList]}>
-                            <Image
-                                source={SettingIcon}
-                                style={SideMenuStyle.navIcon}
+                            <SvgXml
+                                xml={gear}
+                                width={24}
+                                height={24}
                             />
                             <Text style={[SideMenuStyle.menuItemText]}>정보수정</Text>
                         </View>
@@ -87,18 +117,32 @@ const SideMenu = ({closeSide, nowPage}: props) => {
                 </View>
 
                 <View style={SideMenuStyle.authButtonWrap}>
-                    <TouchableOpacity activeOpacity={0.7} onPress={()=>navigation.navigate("Login")}>
-                        <View style={SideMenuStyle.loginButton}>
-                            <Text style={SideMenuStyle.loginButtonText}>로그인</Text>
-                        </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity activeOpacity={0.7} onPress={()=>navigation.navigate("SignUpSeparate")}>
-                        <Text style={SideMenuStyle.signUpButton}>아직 회원이 아니신가요?</Text>
-                    </TouchableOpacity>
+                    {
+                        userStore.user ? (
+                            <>
+                                <TouchableOpacity activeOpacity={0.7} onPress={logout}>
+                                    <View style={SideMenuStyle.loginButton}>
+                                        <Text style={SideMenuStyle.loginButtonText}>로그아웃</Text>
+                                    </View>
+                                </TouchableOpacity>
+                            </>
+                        ):(
+                            <>
+                                <TouchableOpacity activeOpacity={0.7} onPress={()=>navigation.navigate("Login")}>
+                                    <View style={SideMenuStyle.loginButton}>
+                                        <Text style={SideMenuStyle.loginButtonText}>로그인</Text>
+                                    </View>
+                                </TouchableOpacity>
+                                <TouchableOpacity activeOpacity={0.7} onPress={()=>navigation.navigate("SignUpSeparate")}>
+                                    <Text style={SideMenuStyle.signUpButton}>아직 회원이 아니신가요?</Text>
+                                </TouchableOpacity>
+                            </>
+                        )
+                    }
                 </View>
             </View>
         </>
     );
-}
+});
 
 export default SideMenu;
