bfff1aaeb5b16bf526077d1c687b816f70f31e61
왕준영 <wangsun7@naver.com>
2023-11-03 01:54:47 +0900

    :hammer: [BE] Refactor: ResponseSerializer -> ObjectSerializer 로 이름 변경

--- back/help-match/src/main/java/com/ssafy/help/match/socket/controller/HelpMatchSocketController.java
+++ back/help-match/src/main/java/com/ssafy/help/match/socket/controller/HelpMatchSocketController.java
@@ -1,9 +1,7 @@
 package com.ssafy.help.match.socket.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.help.match.socket.response.MatchStatusResponse;
-import com.ssafy.help.match.socket.response.ResponseSerializer;
+import com.ssafy.help.match.util.ObjectSerializer;
 import com.ssafy.help.match.socket.service.HelpMatchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -14,14 +12,14 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequiredArgsConstructor
 public class HelpMatchSocketController{
-    private final ResponseSerializer responseSerializer;
+    private final ObjectSerializer objectSerializer;
     private final SimpMessageSendingOperations simpMessageSendingOperations;
     private final HelpMatchService helpMatchService;
 
     @SubscribeMapping("/topic/match/status/{memberId}")
     void match(@DestinationVariable Long memberId){
         MatchStatusResponse response = helpMatchService.viewStatusByMemberId(memberId);
-        simpMessageSendingOperations.convertAndSend("/topic/match/status/"+memberId,responseSerializer.serialize(response));
+        simpMessageSendingOperations.convertAndSend("/topic/match/status/"+memberId, objectSerializer.serialize(response));
     }
 
 }
---
+++ back/help-match/src/main/java/com/ssafy/help/match/util/ObjectSerializer.java
@@ -0,0 +1,20 @@
+package com.ssafy.help.match.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ObjectSerializer {
+    private final ObjectMapper objectMapper;
+
+    public String serialize(Object response){
+        try {
+            return objectMapper.writeValueAsString(response);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
