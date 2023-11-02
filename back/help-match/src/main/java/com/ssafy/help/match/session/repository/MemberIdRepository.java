04e8767b7aed4aae72ab9c93098e39550daee576
왕준영 <wangsun7@naver.com>
2023-11-02 09:02:54 +0900

    :heavy_plus_sign: [BE] Feat: Redis에 MemberId를 저장하는 기능 추가

---
+++ back/help-match/src/main/java/com/ssafy/help/match/session/repository/MemberIdRepository.java
@@ -0,0 +1,24 @@
+package com.ssafy.help.match.session.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberIdRepository {
+    private final RedisTemplate<String,String> redisTemplate;
+    private final String PREFIX="sessionId:";
+
+    public void save(String sessionId,Long memberId){
+        redisTemplate.opsForValue().set(PREFIX+sessionId, memberId.toString());
+    }
+
+    public Long getMemberId(String sessionId){
+        return Long.parseLong(redisTemplate.opsForValue().get(PREFIX+sessionId));
+    }
+
+    public Long deleteAndGetMemberId(String sessionId){
+        return Long.parseLong(redisTemplate.opsForValue().getAndDelete(PREFIX+sessionId));
+    }
+}
