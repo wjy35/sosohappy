20a4943d5886aafb0dcab6c8266ce5b89a8a3e3d
youn <yyytae0@gmail.com>
2023-11-02 16:52:28 +0900

    :heavy_plus_sign: [FE] Feat: add logic to send position in help match api

--- front/src/apis/helpMatchApi.ts
+++ front/src/apis/helpMatchApi.ts
@@ -2,11 +2,22 @@ import {PrivateInstance, PublicInstance} from "@/apis/AXIOSUTILS";
 
 const domain = 'help-match';
 
-interface props {
-
+interface propsType {
+    latitude: number;
+    longitude: number;
 }
 
 const helpMatchApi = {
+    sendPosition: async ({latitude, longitude}: propsType) => {
+        const res = PrivateInstance.post(
+            `${domain}/point`,
+            {
+                latitude: latitude,
+                longitude: longitude,
+            }
+        );
+        return res;
+    },
 
 };
 
