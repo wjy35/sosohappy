af6b0f3d8b10db1ca97bc90dd0073a12c13d3d80
seokJKIM <tjrwn1247@naver.com>
2023-11-03 09:52:47 +0900

    :heavy_plus_sign: [BE] Feat: ChatRoom Repository 추가

---
+++ back/chat/src/main/java/com/ssafy/chat/db/repository/ChatRoomRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.chat.db.repository;
+
+import com.ssafy.chat.db.entity.ChatRoomEntity;
+import com.ssafy.chat.db.entity.ChatRoomEntityPK;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoomEntity, ChatRoomEntityPK> {
+}
