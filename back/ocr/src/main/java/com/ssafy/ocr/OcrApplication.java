26ffd8d67d024b3b8905409fe70de2e2acd00d11
김성재 <tjdwo6269@naver.com>
2023-11-06 14:42:03 +0900

    :construction_worker: [BE] Setting: ocr 서버 EnableDiscoveryClient 추가

--- back/ocr/src/main/java/com/ssafy/ocr/OcrApplication.java
+++ back/ocr/src/main/java/com/ssafy/ocr/OcrApplication.java
@@ -2,8 +2,11 @@ package com.ssafy.ocr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OcrApplication {
 
 	public static void main(String[] args) {
