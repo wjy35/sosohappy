7202a63c4ea4e5159fe1e015a43efb3394176760
김성재 <tjdwo6269@naver.com>
2023-11-06 17:19:41 +0900

    :heavy_plus_sign: [BE] Feat: 네이버 api 호출 메서드 작성

--- back/ocr/src/main/java/com/ssafy/ocr/api/service/OcrService.java
+++ back/ocr/src/main/java/com/ssafy/ocr/api/service/OcrService.java
@@ -30,6 +30,60 @@ public class OcrService {
     @Value("${ocr.secret.key}")
     private String secretKey;
 
+    private String naverOcrApi(String base64EncodeFile, String ext) {
+
+        String apiURL = this.apiURL;
+        String secretKey = this.secretKey;
+        String result = null;
+        try {
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setUseCaches(false);
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+            con.setRequestProperty("X-OCR-SECRET", secretKey);
+
+            JSONObject json = new JSONObject();
+            json.put("version", "V2");
+            json.put("requestId", UUID.randomUUID().toString());
+            json.put("timestamp", System.currentTimeMillis());
+            JSONObject image = new JSONObject();
+            image.put("format", ext);
+            image.put("data", base64EncodeFile); // image should be public, otherwise, should use data
+            image.put("name", "demo");
+            JSONArray images = new JSONArray();
+            images.put(image);
+            json.put("images", images);
+            String postParams = json.toString();
+
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(postParams);
+            wr.flush();
+            wr.close();
+
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if (responseCode == 200) {
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            result = response.toString();
+            br.close();
+
+        } catch (Exception e) {
+            e.getMessage();
+        }
+
+        return result;
+    }
     public String base64Encoding(MultipartFile multipartFile) throws IOException {
 
         byte[] bytes = multipartFile.getBytes();
