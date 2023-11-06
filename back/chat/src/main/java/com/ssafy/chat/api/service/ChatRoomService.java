ccc846e4eae9c1b4d5da07fc461441b301473608
seokJKIM <tjrwn1247@naver.com>
2023-11-06 13:47:54 +0900

    :heavy_plus_sign: [BE] Feat: add getChatRoomList 메서드

--- back/chat/src/main/java/com/ssafy/chat/api/service/ChatRoomService.java
+++ back/chat/src/main/java/com/ssafy/chat/api/service/ChatRoomService.java
@@ -1,8 +1,16 @@
 package com.ssafy.chat.api.service;
 
 import com.ssafy.chat.api.request.ChatRoomCreateRequest;
+import com.ssafy.chat.api.response.ChatRoomListParam;
+import com.ssafy.chat.db.entity.ChatRoomEntity;
+
+import java.util.List;
 
 public interface ChatRoomService {
 
     Integer creatChatRoom(ChatRoomCreateRequest chatRoomCreateRequest);
+
+    List<ChatRoomListParam> getChatRoomListParams(long memberId);
+
+    List<ChatRoomEntity> getChatRoomList(long memberId);
 }
--- back/chat/src/main/java/com/ssafy/chat/api/service/impl/ChatRoomServiceImpl.java
+++ back/chat/src/main/java/com/ssafy/chat/api/service/impl/ChatRoomServiceImpl.java
@@ -2,12 +2,16 @@ package com.ssafy.chat.api.service.impl;
 
 import com.ssafy.chat.api.mapper.ChatRoomMapper;
 import com.ssafy.chat.api.request.ChatRoomCreateRequest;
+import com.ssafy.chat.api.response.ChatParam;
+import com.ssafy.chat.api.response.ChatRoomListParam;
 import com.ssafy.chat.api.service.ChatRoomService;
 import com.ssafy.chat.db.entity.ChatRoomEntity;
 import com.ssafy.chat.db.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -27,4 +31,31 @@ public class ChatRoomServiceImpl implements ChatRoomService {
             return chatRoomEntityOption.get().getChatRoomId();
         }
     }
+
+    @Override
+    public List<ChatRoomListParam> getChatRoomListParams(long memberId) {
+        List<ChatRoomListParam> chatRoomListParams = new ArrayList<>();
+
+        List<ChatRoomEntity> chatRoomList = getChatRoomList(memberId);
+
+        for(int i=0; i<chatRoomList.size(); i++){
+
+            ChatRoomEntity chatRoomEntity = chatRoomList.get(i);
+
+            long chatPartnerMemberId = memberId == chatRoomEntity.getReceiverMemberId()?chatRoomEntity.getSenderMemberId():chatRoomEntity.getReceiverMemberId();
+
+            // ChatParam redis에서 memberId로 마지막 채팅 가져와
+//            ChatParam chatParam =
+
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<ChatRoomEntity> getChatRoomList(long memberId) {
+        return chatRoomRepository.findByReceiverMemberIdOrSenderMemberId(memberId, memberId);
+    }
+
+
 }
