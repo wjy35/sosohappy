9636c42c29a257f4626c29cac8b128b29f031f8b
youn <yyytae0@gmail.com>
2023-11-07 17:14:46 +0900

    :sparkles: [FE] Style: set backdrop opacity more dark and set zIndex bigger

--- front/src/styles/MapLoadingStyle.tsx
+++ front/src/styles/MapLoadingStyle.tsx
@@ -6,8 +6,9 @@ const MapLoadingStyle = StyleSheet.create({
         width:"100%",
         height:"100%",
         backgroundColor:"#000",
-        opacity:0.5,
+        opacity:0.8,
         position:"absolute",
+        zIndex: 10,
     },
     modalMain:{
         width:responsiveWidth(80),
@@ -17,6 +18,7 @@ const MapLoadingStyle = StyleSheet.create({
         display:"flex",
         justifyContent:"center",
         alignItems:"center",
+        zIndex: 11,
     },
     turtleLoading:{
         width:280,
