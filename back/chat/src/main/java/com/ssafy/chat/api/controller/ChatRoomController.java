c61fa9eda05a39317f00a0b29b437a25a50a6474
seokJKIM <tjrwn1247@naver.com>
2023-11-07 17:55:36 +0900

    :heavy_plus_sign: [BE] Feat: add getChatRoomList

--- back/chat/src/main/java/com/ssafy/chat/api/controller/ChatRoomController.java
+++ back/chat/src/main/java/com/ssafy/chat/api/controller/ChatRoomController.java
@@ -1,14 +1,19 @@
 package com.ssafy.chat.api.controller;
 
 import com.ssafy.chat.api.request.ChatRoomCreateRequest;
+import com.ssafy.chat.api.response.ChatRoomListParam;
 import com.ssafy.chat.api.response.FormattedResponse;
 import com.ssafy.chat.api.service.ChatRoomService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@CrossOrigin("*")
 @RequiredArgsConstructor
 public class ChatRoomController {
 
@@ -27,9 +32,15 @@ public class ChatRoomController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @GetMapping("/chatroom")
+    public ResponseEntity<?> getChatRoomList(@RequestHeader("memberId") long memberId){
+        List<ChatRoomListParam> chatRoomListParams;
 
-//    @GetMapping("/chatroom")
-//    public ResponseEntity<?> getChatRoomList(@RequestHeader("memberId") long memberId){
-//
-//    }
+        FormattedResponse response = FormattedResponse.builder()
+                .status("success")
+                .message("GET CHATROOM lIST")
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
