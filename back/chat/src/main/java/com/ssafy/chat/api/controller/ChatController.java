77e897555584e4c459c507f657a108de523077ef
seokJKIM <tjrwn1247@naver.com>
2023-11-08 13:03:49 +0900

    :heavy_plus_sign: [BE] Feat: add 채팅 저장

--- back/chat/src/main/java/com/ssafy/chat/api/controller/ChatController.java
+++ back/chat/src/main/java/com/ssafy/chat/api/controller/ChatController.java
@@ -1,6 +1,8 @@
 package com.ssafy.chat.api.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ssafy.chat.api.request.ChatPayload;
+import com.ssafy.chat.api.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -14,8 +16,16 @@ public class ChatController {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
 
+    private final ChatService chatService;
+
     @MessageMapping(value = "/test")
     public void message(ChatPayload chatPayload){
+
+        try {
+            chatService.saveChat(chatPayload);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println(chatPayload.toString());
         simpMessagingTemplate.convertAndSend("/topic/chat", chatPayload);
     }
