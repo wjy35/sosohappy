78fc392e13fc99985842e0f3db2f780c629b568d
seokJKIM <tjrwn1247@naver.com>
2023-11-08 17:28:26 +0900

    :heavy_plus_sign: [BE] Feat: Controller createChatRoom 추가

--- back/chat/src/main/java/com/ssafy/chat/api/controller/ChatRoomController.java
+++ back/chat/src/main/java/com/ssafy/chat/api/controller/ChatRoomController.java
@@ -1,11 +1,11 @@
 package com.ssafy.chat.api.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ssafy.chat.api.request.ChatRoomCreateRequest;
-import com.ssafy.chat.api.response.ChatRoomListParam;
+import com.ssafy.chat.api.response.ChatRoomList;
 import com.ssafy.chat.api.response.FormattedResponse;
 import com.ssafy.chat.api.service.ChatRoomService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -34,11 +34,18 @@ public class ChatRoomController {
 
     @GetMapping("/chatroom")
     public ResponseEntity<?> getChatRoomList(@RequestHeader("memberId") long memberId){
-        List<ChatRoomListParam> chatRoomListParams;
+
+        List<ChatRoomList> chatRoomLists;
+        try {
+            chatRoomLists = chatRoomService.getChatRoomListParams(memberId);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
 
         FormattedResponse response = FormattedResponse.builder()
                 .status("success")
                 .message("GET CHATROOM lIST")
+                .result("chatRoomList",chatRoomLists)
                 .build();
 
         return new ResponseEntity<>(response, HttpStatus.OK);
