2018b45f0381ce947e98b81588e56340465954f4
seokJKIM <tjrwn1247@naver.com>
2023-11-08 17:21:28 +0900

    :hammer: [BE] Refactor: getChatRoomListParams return type 변경

--- back/chat/src/main/java/com/ssafy/chat/api/service/ChatRoomService.java
+++ back/chat/src/main/java/com/ssafy/chat/api/service/ChatRoomService.java
@@ -1,7 +1,8 @@
 package com.ssafy.chat.api.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ssafy.chat.api.request.ChatRoomCreateRequest;
-import com.ssafy.chat.api.response.ChatRoomListParam;
+import com.ssafy.chat.api.response.ChatRoomList;
 import com.ssafy.chat.db.entity.ChatRoomEntity;
 
 import java.util.List;
@@ -10,7 +11,7 @@ public interface ChatRoomService {
 
     Integer creatChatRoom(ChatRoomCreateRequest chatRoomCreateRequest);
 
-    List<ChatRoomListParam> getChatRoomListParams(long memberId);
+    List<ChatRoomList> getChatRoomListParams(long memberId) throws JsonProcessingException;
 
     List<ChatRoomEntity> getChatRoomList(long memberId);
 }
