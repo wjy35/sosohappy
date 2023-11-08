bb5f4e326a8ed4424d2839a8a3ada23cc0113b71
livinoid98 <ccy9803@gmail.com>
2023-11-08 10:32:05 +0900

    :art: [FE] Design(certificate-publish): 증명서 발급 페이지 UI 퍼블리싱 완료
    - 발급 시 재로그인 요청 (web상에서 accessToken 새로 발급 및 갱신 위해)
    - 발급 인증서 페이지 생성

---
+++ web/src/routes/docs/certificate/list/+page.svelte
@@ -0,0 +1,91 @@
+<script>
+
+</script>
+
+<div>
+    <table border=1>
+        <tr>
+            <td colspan="3">봉사활동 확인서</td>
+        </tr>
+        <tr>
+            <td rowspan="4">인적사항</td>
+        </tr>
+        <tr>
+            <td>이름</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>생년월일</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>성별</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td colspan="3">봉사내역</td>
+        </tr>
+        <tr>
+            <td>도와준 사람</td>
+            <td>카테고리</td>
+            <td>날짜</td>
+        </tr>
+        <tr>
+            <td>왕준영</td>
+            <td>보행</td>
+            <td>2023.11.07</td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                위 사람의 소중한 도움제공 내역을 소소행에서 발행해드립니다.
+                2023.11.07.
+            </td>
+        </tr>
+    </table>
+</div>
+
+<style>
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+</style>
