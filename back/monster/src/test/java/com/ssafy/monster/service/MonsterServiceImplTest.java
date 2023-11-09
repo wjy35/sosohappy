d43b658b29fc9cbc8869644abf72311ad58850fb
김성재 <tjdwo6269@naver.com>
2023-11-09 14:47:32 +0900

    :white_check_mark: [BE] Test: monster 클로버가 이미 꽉차 있는 경우 test 작성

--- back/monster/src/test/java/com/ssafy/monster/service/MonsterServiceImplTest.java
+++ back/monster/src/test/java/com/ssafy/monster/service/MonsterServiceImplTest.java
@@ -1,8 +1,13 @@
 package com.ssafy.monster.service;
 
+import com.netflix.discovery.shared.Pair;
 import com.ssafy.monster.common.exception.CustomException;
 import com.ssafy.monster.common.exception.ErrorCode;
+import com.ssafy.monster.domain.entity.MemberMonsterGrowth;
 import com.ssafy.monster.domain.entity.MemberMonsterProfile;
+import com.ssafy.monster.domain.entity.MonsterInfo;
+import com.ssafy.monster.repository.GrowthRepository;
+import com.ssafy.monster.repository.InfoRepository;
 import com.ssafy.monster.repository.ProfileRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +15,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import javax.annotation.PostConstruct;
+import java.text.ChoiceFormat;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -19,6 +28,31 @@ class MonsterServiceImplTest {
 
     @Autowired
     private ProfileRepository profileRepository;
+
+    @Autowired
+    private InfoRepository infoRepository;
+
+    @Autowired
+    private GrowthRepository growthRepository;
+
+    @Autowired
+    private MonsterService monsterService;
+
+    private double[] expArr; //누적 경험치
+    private String[] levelArr;
+
+    private ChoiceFormat cf;
+
+    @PostConstruct
+    private void getExpinfo() {
+        List<Integer> expList = infoRepository.getMonsterCloverInfo();
+        expArr = expList.stream().mapToDouble(i -> i).toArray();
+
+        List<Integer> levelList = infoRepository.getMonsterLevels();
+        levelArr = levelList.stream().map(i -> i.toString()).toArray(size -> new String[size]);
+
+        cf = new ChoiceFormat(expArr, levelArr);
+    }
 
     @Test
     @DisplayName("id에 존재하는 MemberMonsterProfile이 있는지 확인한다.")
@@ -45,6 +79,25 @@ class MonsterServiceImplTest {
                 .isInstanceOf(CustomException.class)
                 .hasMessage(ErrorCode.MEMBER_NOT_FOUND.getMessage());
     }
+
+    @Test
+    @DisplayName("이미 클로버가 꽉 차있는 상태에서(만렙) 레벨업 요청시 예외 발생시킨다.")
+    void throwExceptionWhenCloverIsFull() {
+
+        // given : 78개의 클로버를 가지고 있는 id
+        MemberMonsterGrowth growth = growthRepository.findByMemberMonsterId(447L)
+                .orElseThrow(() -> new CustomException(ErrorCode.GROWTH_NOT_FOUND));
+
+
+        // when / then
+        Assertions.assertThatThrownBy(() -> monsterService.updateMonsterClover
+                (447L, 1))
+                .isInstanceOf(CustomException.class)
+                .hasMessage(ErrorCode.FULL_OF_CLOVER.getMessage());
+
+    }
+
+
 
 
 }
