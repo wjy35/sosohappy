371451dfd7ff67f7a531c6d58ed1a49cfc1f10f1
seokJKIM <tjrwn1247@naver.com>
2023-11-10 15:44:23 +0900

    :heavy_plus_sign: [BE] Feat: ChatMapper createChatPublish 메서드 추가

--- back/chat/src/main/java/com/ssafy/chat/api/mapper/ChatMapper.java
+++ back/chat/src/main/java/com/ssafy/chat/api/mapper/ChatMapper.java
@@ -1,9 +1,8 @@
 package com.ssafy.chat.api.mapper;
 
 import com.ssafy.chat.api.request.ChatPayload;
-import com.ssafy.chat.api.request.ChatRoomCreateRequest;
+import com.ssafy.chat.api.request.ChatPublish;
 import com.ssafy.chat.db.entity.ChatEntity;
-import com.ssafy.chat.db.entity.ChatRoomEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -13,4 +12,6 @@ public interface ChatMapper {
     ChatMapper INSTANCE = Mappers.getMapper(ChatMapper.class);
 
     ChatEntity createRequestToEntity(ChatPayload chatPayload);
+
+    ChatPublish createChatPublish(ChatPayload chatPayload);
 }
