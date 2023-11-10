e2854eb0658ba8307cf8131b6227fbf399a16464
정민희 <mong2072@naver.com>
2023-11-10 13:50:34 +0900

    :bug: [BE] Fix: 서버 driver 경로 수정

--- back/ocr/src/main/java/com/ssafy/ocr/config/ChromeConfig.java
+++ back/ocr/src/main/java/com/ssafy/ocr/config/ChromeConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class ChromeConfig {
     @Bean
     public WebDriver webDriver() {
-        System.setProperty("webdriver.chrome.driver", "/app/src/main/resources/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/resources/chromedriver");
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
