e613568b674f31780a08acd3c5a7cae8634878e9
왕준영 <wangsun7@naver.com>
2023-11-10 14:15:23 +0900

    :heavy_plus_sign: [BE] Feat: Fortune Cookie use Service에 use 시 kafka 에 event 발행 기능 추가

--- back/help-match/src/main/java/com/ssafy/help/match/socket/service/impl/FortuneCookieServiceImpl.java
+++ back/help-match/src/main/java/com/ssafy/help/match/socket/service/impl/FortuneCookieServiceImpl.java
@@ -2,6 +2,8 @@ package com.ssafy.help.match.socket.service.impl;
 
 import com.ssafy.help.match.db.entity.FortuneCookieEntity;
 import com.ssafy.help.match.db.repository.FortuneCookieEntityRepository;
+import com.ssafy.help.match.event.producer.KafkaEventProducer;
+import com.ssafy.help.match.event.producer.KafkaEventTopic;
 import com.ssafy.help.match.socket.service.FortuneCookieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FortuneCookieServiceImpl implements FortuneCookieService {
     private final FortuneCookieEntityRepository fortuneCookieEntityRepository;
+    private final KafkaEventProducer kafkaEventProducer;
 
     @Override
     public List<FortuneCookieEntity> viewList(Long memberId) {
@@ -20,5 +23,7 @@ public class FortuneCookieServiceImpl implements FortuneCookieService {
     @Override
     public void use(Long memberId, String fortuneCookieId) {
         fortuneCookieEntityRepository.delete(memberId,fortuneCookieId);
+
+        kafkaEventProducer.produce(KafkaEventTopic.FORTUNE_COOKIE_USE,memberId.toString());
     }
 }
