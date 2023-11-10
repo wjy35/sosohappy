b9adc419e29d2c121867f41cac0bc6db0c18a8ad
seokJKIM <tjrwn1247@naver.com>
2023-11-10 16:21:47 +0900

    :hammer: [BE] Refactor: ChatServerIdRepositoryImpl throws 변경

--- back/chat/src/main/java/com/ssafy/chat/api/service/ChatSaveService.java
+++ back/chat/src/main/java/com/ssafy/chat/api/service/ChatSaveService.java
@@ -1,8 +1,7 @@
 package com.ssafy.chat.api.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ssafy.chat.db.entity.ChatEntity;
 
 public interface ChatSaveService {
-    void saveChat(ChatEntity chatEntity, int roomId) throws JsonProcessingException;
+    void saveChat(ChatEntity chatEntity, int roomId);
 }
