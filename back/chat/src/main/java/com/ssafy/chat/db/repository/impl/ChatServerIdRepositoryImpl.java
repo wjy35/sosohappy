22690c80a185aaad58fac97d7bb33993bcf03839
seokJKIM <tjrwn1247@naver.com>
2023-11-10 15:41:25 +0900

    :heavy_plus_sign: [BE] Feat: ChatServerIdRepositoryImpl saveByMemberId, deleteByMemberId, getChatServerIdByMemberId 추가

---
+++ back/chat/src/main/java/com/ssafy/chat/db/repository/impl/ChatServerIdRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.ssafy.chat.db.repository.impl;
+
+import com.ssafy.chat.config.ChatServerInfo;
+import com.ssafy.chat.db.repository.ChatServerIdRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class ChatServerIdRepositoryImpl implements ChatServerIdRepository {
+    @Value("${CHAT_SERVER_ID_REPOSITORY_PREFIX}")
+    String chatServerIdPrefix;
+
+    private final ChatServerInfo chatServerInfo;
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void saveByMemberId(Long memberId){
+        System.out.println(memberId);
+        System.out.println(chatServerIdPrefix+memberId);
+        redisTemplate.opsForValue().set(chatServerIdPrefix+memberId, chatServerInfo.getId());
+    }
+
+    public void deleteByMemberId(Long memberId){
+        redisTemplate.opsForValue().getAndDelete(chatServerIdPrefix+memberId);
+    }
+
+    public String getChatServerIdByMemberId(Long memberId){
+        return redisTemplate.opsForValue().get(chatServerIdPrefix+memberId);
+    }
+}
