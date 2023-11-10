10e9279aace3ffd954301735dfa8da2526314e08
왕준영 <wangsun7@naver.com>
2023-11-10 14:04:00 +0900

    :heavy_plus_sign: [BE] Feat: FortuneCookie List 조회 API 추가

---
+++ back/help-match/src/main/java/com/ssafy/help/match/api/controller/FortuneCookieController.java
@@ -0,0 +1,34 @@
+package com.ssafy.help.match.api.controller;
+
+import com.ssafy.help.match.api.response.FormattedResponse;
+import com.ssafy.help.match.socket.mapper.FortuneCookieMapper;
+import com.ssafy.help.match.socket.response.FortuneCookieItem;
+import com.ssafy.help.match.socket.service.FortuneCookieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class FortuneCookieController {
+    private final FortuneCookieService fortuneCookieService;
+
+    @GetMapping("/fortune-cookie/view")
+    ResponseEntity<FormattedResponse> view(@RequestHeader Long memberId){
+        List<FortuneCookieItem> fortuneCookieItemList = FortuneCookieMapper.INSTANCE
+                .toItem(fortuneCookieService.viewList(memberId));
+
+        FormattedResponse response = FormattedResponse
+                .builder()
+                .status("success")
+                .message("SUCCESS GET LIST")
+                .result("fortuneCookieList",fortuneCookieItemList)
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
