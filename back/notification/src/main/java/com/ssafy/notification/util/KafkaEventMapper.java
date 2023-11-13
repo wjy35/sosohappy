211895a30adc54fc8a401aa3f2078cfa29cadd20
왕준영 <wangsun7@naver.com>
2023-11-13 17:10:54 +0900

    :heavy_plus_sign: [BE] Feat: ObjectSerializer, KafkaEventMapper Util 추가

---
+++ back/notification/src/main/java/com/ssafy/notification/util/KafkaEventMapper.java
@@ -0,0 +1,23 @@
+package com.ssafy.notification.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class KafkaEventMapper {
+    private final ObjectMapper objectMapper;
+
+    public <T>T toEvent(ConsumerRecord<?,?> consumerRecord,Class<T> eventClass){
+        String eventMessage = (String)consumerRecord.value();
+
+        try {
+            return objectMapper.readValue(eventMessage,eventClass);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
