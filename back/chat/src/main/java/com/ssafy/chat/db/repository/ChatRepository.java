6dd73097dffb38bdacc6c67a5021bd9208d0ac8b
seokJKIM <tjrwn1247@naver.com>
2023-11-13 11:21:37 +0900

    :heavy_plus_sign: [BE] Feat: ChatRepository  getChatList 메서드 추가

--- back/chat/src/main/java/com/ssafy/chat/db/repository/ChatRepository.java
+++ back/chat/src/main/java/com/ssafy/chat/db/repository/ChatRepository.java
@@ -2,8 +2,12 @@ package com.ssafy.chat.db.repository;
 
 import com.ssafy.chat.db.entity.ChatEntity;
 
+import java.util.List;
+
 public interface ChatRepository {
     void saveChat(ChatEntity chatEntity, int roomId);
 
     ChatEntity getLastChat(int chatRoomId);
+
+    List<ChatEntity> getChatList(int chatRoomId);
 }
