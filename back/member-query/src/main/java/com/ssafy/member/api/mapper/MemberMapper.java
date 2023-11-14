61c8149d13f3a4989c6b5807d19d04c61a681421
왕준영 <wangsun7@naver.com>
2023-11-14 09:59:14 +0900

    :heavy_plus_sign: [BE] Feat: member public info API 추가

--- back/member-query/src/main/java/com/ssafy/member/api/mapper/MemberMapper.java
+++ back/member-query/src/main/java/com/ssafy/member/api/mapper/MemberMapper.java
@@ -1,6 +1,7 @@
 package com.ssafy.member.api.mapper;
 
 import com.ssafy.member.api.response.MemberInformationResponse;
+import com.ssafy.member.api.response.MemberPublicInformationResponse;
 import com.ssafy.member.db.entity.MemberEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -10,4 +11,5 @@ public interface MemberMapper {
     MemberMapper INSTANCE = Mappers.getMapper(MemberMapper.class);
 
     MemberInformationResponse entityToResponse(MemberEntity memberEntity);
+    MemberPublicInformationResponse toPublicResponse(MemberEntity memberEntity);
 }
