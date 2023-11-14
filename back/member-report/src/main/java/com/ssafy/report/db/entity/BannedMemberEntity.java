fd904607bfe2e8cd3d3d112637f83ddf2fbc587c
왕준영 <wangsun7@naver.com>
2023-11-15 08:38:56 +0900

    :heavy_plus_sign: [BE] Feat: Mysql에 Banned Member을 관리하는 기능 추가

---
+++ back/member-report/src/main/java/com/ssafy/report/db/entity/BannedMemberEntity.java
@@ -0,0 +1,22 @@
+package com.ssafy.report.db.entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "banned_member")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class BannedMemberEntity {
+    @Id
+    @Column(name = "member_id")
+    Long memberId;
+
+    @Column(name = "end_timestamp")
+    Timestamp endTimestamp;
+}
