0353316aae6c7552b9af918cd6e538dd0ff1e90a
youn <yyytae0@gmail.com>
2023-11-15 14:59:27 +0900

    :heavy_plus_sign: [FE] Feat: set variable for chat info

--- front/src/components/ChatListItem.tsx
+++ front/src/components/ChatListItem.tsx
@@ -2,6 +2,11 @@ import { View, Text, Image, TouchableOpacity } from "react-native"
 import { useNavigation } from "@react-navigation/native";
 
 import ChatListItemStyle from "@/styles/ChatListItemStyle"
+import memberApi from "@/apis/memberApi";
+import {useEffect, useState} from "react";
+import {type1, type2, type3, type4} from "@/assets/sosomon";
+import {SingleChatInfo} from "@/types";
+import useStore from "@/store/store";
 
 enum Rank{
     '나눔이',
@@ -9,33 +14,30 @@ enum Rank{
 }
 
 interface propsType {
-    thumbnail: HTMLImageElement,
-    name: string,
-    rank: Rank,
-    recentMessage: string,
+    chatInfo: SingleChatInfo
 }
 
-const ChatListItem = ({thumbnail, name, rank, recentMessage} :propsType) => {
+const ChatListItem = ({chatInfo} :propsType) => {
     const navigation = useNavigation();
     return(
         <>
-            <TouchableOpacity activeOpacity={0.7} onPress={() => navigation.navigate('Chat')}>
+            <TouchableOpacity activeOpacity={0.7} onPress={() => navigation.navigate('Chat', {otherMemberId: otherMemberInfo.memberId})}>
                 <View style={ChatListItemStyle.chatListItemWrap}>
                     <Image
-                        source={thumbnail}
+                        source={src?src:type4[0]}
                         style={ChatListItemStyle.chatListItemThumbnailImg}
                     />
                     <View style={ChatListItemStyle.chatContentWrap}>
                         <View style={ChatListItemStyle.chatInfoWrap}>
-                            <Text style={ChatListItemStyle.chatInfoName}>{name}</Text>
+                            <Text style={ChatListItemStyle.chatInfoName}>{otherMemberInfo?.nickname}</Text>
                             <View style={ChatListItemStyle.charInfoRank}>
-                                <Text style={ChatListItemStyle.chatInfoRankText}>{rank}</Text>
+                                <Text style={ChatListItemStyle.chatInfoRankText}>{otherMemberInfo?.disabled?'나눔이':'모음이'}</Text>
                             </View>
                             <View style={ChatListItemStyle.msgAlarm}>
                                 <Text style={ChatListItemStyle.msgAlarmText}>1</Text>
                             </View>
                         </View>
-                        <Text style={ChatListItemStyle.recentMessageText}>{recentMessage}</Text>
+                        <Text style={ChatListItemStyle.recentMessageText}>{chatInfo.currentChat.content}</Text>
                     </View>
                 </View>
             </TouchableOpacity>
