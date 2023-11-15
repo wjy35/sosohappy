f421ffef954ecd857e8b7b4c303980f2c669b774
youn <yyytae0@gmail.com>
2023-11-15 14:54:38 +0900

    :hammer: [FE] Refactor: fix scrollview to flatlist and remove commonlayout to remove scrollview

--- front/src/screens/Chat.tsx
+++ front/src/screens/Chat.tsx
@@ -85,94 +85,83 @@ const Chat = ({helpSocket, chatSocket}: propsType) => {
   )
 
   return (
-    <CommonLayout footer={false} headerType={1}>
-      <View style={ChatStyle.heightWrap}>
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView
+            edges={["top", "right", "bottom", "left"]}
+            style={{flex: 1}}
+        >
+          <Header headerType={1} openSide={openSide}/>
+          <SideMenu closeSide={closeSide} nowPage={'Chat'} isVisible={isVisible}/>
+          <View>
+            <View style={ChatStyle.heightWrap}>
+              <View style={ChatStyle.chatContentWrap}>
+                <FlatList
+                    data={[...chatSocket.msgList].reverse()}
+                    showsVerticalScrollIndicator={false}
+                    windowSize={3}
+                    inverted={true}
+                    initialNumToRender={5}
+                    renderItem={(item) => {
+                      if (item.item.memberId === userInfo.memberId){
+                        return <MyChat content={item.item.content}/>
+                      } else if (item.item.memberId === otherMemberId) {
+                        return <YourChat thumbnail={FishThumbnail} content={item.item.content}/>
+                      } else {
+                        return <Text>{item.item.content}</Text>
+                      }
+                    }}
+                    keyExtractor={(item) => String(item.timestamp)+String(item.memberId)+String(item.content)}
+                    onEndReachedThreshold={0.1}
+                    contentContainerStyle={{paddingHorizontal: 10}}
+                />
+              </View>
+              <View style={ChatStyle.agreeWrap}>
+                <SvgXml
+                    xml={people}
+                    width={45}
+                    height={45}
+                />
+                <Text style={ChatStyle.agreeInfoText}>매칭하기 버튼을 통해 상대방의 요청을 승인해주세요.</Text>
+                <TouchableOpacity activeOpacity={0.7}>
+                  <View style={ChatStyle.agreeButton}>
+                    <Text style={ChatStyle.agreeButtonText}>승인하기</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
 
-        {/* <View style={ChatStyle.chatTitleWrap}>
-          <TouchableOpacity activeOpacity={0.7}>
-            <SvgXml
-              xml={backIcon}
-              width={32}
-              height={32}
-            />
-          </TouchableOpacity>
-          <View style={ChatStyle.chatTitle}>
-            <Text style={ChatStyle.chatTitleName}>김싸피</Text>
-            <Text style={ChatStyle.chatTitleRank}>나눔이</Text>
+              <View style={ChatStyle.controlWrap}>
+                <TouchableOpacity activeOpacity={0.7}>
+                  <View style={ChatStyle.exitFlexWrap}>
+                    <SvgXml
+                        xml={exit}
+                        width={20}
+                        height={20}
+                    />
+                    <Text style={ChatStyle.exitText}>채팅방 나가기</Text>
+                  </View>
+                </TouchableOpacity>
+                <TextInput
+                    placeholder="메시지 내용을 입력하세요."
+                    style={ChatStyle.chanMsgInput}
+                    onChangeText={(text) => setMsg(text)}
+                    value={msg}
+                />
+                <TouchableOpacity activeOpacity={0.7} onPress={sendMsg}>
+                  <View style={ChatStyle.sendButton}>
+                    <SvgXml
+                        xml={send2}
+                        width={20}
+                        height={20}
+                    />
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
           </View>
-          <TouchableOpacity activeOpacity={0.7}>
-            <SvgXml
-              xml={hamburgerMenu}
-              width={30}
-              height={30}
-            />
-          </TouchableOpacity>
-        </View> */}
-
-        <View style={ChatStyle.chatContentWrap}>
-          <ScrollView>
-            <ChatDate date="2023. 10. 15."/>
-            {
-              msgList.map((msg, index) => {
-                return(
-                  <>
-                    {
-                      String(msg.memberId) === yourMemberId ?
-                      <YourChat thumbnail={FishThumbnail} content={msg.content} key={index}/>
-                      :
-                      <MyChat content={msg.content} key={index}/>
-                    }
-                  </>
-                );
-              })
-            }
-
-          </ScrollView>
-        </View>
-
-        <View style={ChatStyle.agreeWrap}>
-          <SvgXml
-            xml={people}
-            width={45}
-            height={45}
-          />
-          <Text style={ChatStyle.agreeInfoText}>매칭하기 버튼을 통해 상대방의 요청을 승인해주세요.</Text>
-          <TouchableOpacity activeOpacity={0.7}>
-            <View style={ChatStyle.agreeButton}>
-              <Text style={ChatStyle.agreeButtonText}>승인하기</Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-
-        <View style={ChatStyle.controlWrap}>
-          <TouchableOpacity activeOpacity={0.7}>
-            <View style={ChatStyle.exitFlexWrap}>
-              <SvgXml
-                xml={exit}
-                width={20}
-                height={20}
-              />
-              <Text style={ChatStyle.exitText}>채팅방 나가기</Text>
-            </View>
-          </TouchableOpacity>
-          <TextInput
-            placeholder="메시지 내용을 입력하세요."
-            style={ChatStyle.chanMsgInput}
-            onChangeText={(text) => setMsg(text)}
-            value={msg}
-          />
-          <TouchableOpacity activeOpacity={0.7} onPress={sendMsg}>
-            <View style={ChatStyle.sendButton}>
-              <SvgXml
-                xml={send2}
-                width={20}
-                height={20}
-              />
-            </View>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </CommonLayout>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </>
   );
 };
 
