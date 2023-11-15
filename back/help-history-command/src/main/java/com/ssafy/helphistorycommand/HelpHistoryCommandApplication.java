004dbfd68657ef04417fc064eb3c825bae66ffa8
seokJKIM <tjrwn1247@naver.com>
2023-11-15 09:21:50 +0900

    :heavy_plus_sign: [BE] Feat: 어노테이션 추가

--- back/help-history-command/src/main/java/com/ssafy/helphistorycommand/HelpHistoryCommandApplication.java
+++ back/help-history-command/src/main/java/com/ssafy/helphistorycommand/HelpHistoryCommandApplication.java
@@ -2,8 +2,12 @@ package com.ssafy.helphistorycommand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class HelpHistoryCommandApplication {
 
     public static void main(String[] args) {
