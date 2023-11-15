43050ff37152d3e85619b0c1d529ed434384ef29
seokJKIM <tjrwn1247@naver.com>
2023-11-15 10:50:28 +0900

    :hammer: [BE] Refactor: HelpCertificate redis 삭제

--- back/help-history-query/src/main/java/com/ssafy/helphistoryquery/db/repository/HelpHistoryRepository.java
+++ back/help-history-query/src/main/java/com/ssafy/helphistoryquery/db/repository/HelpHistoryRepository.java
@@ -1,6 +1,5 @@
 package com.ssafy.helphistoryquery.db.repository;
 
-import com.ssafy.helphistoryquery.db.entity.HelpCertificateEntity;
 import com.ssafy.helphistoryquery.db.entity.HelpHistoryEntity;
 
 import java.util.List;
@@ -14,9 +13,4 @@ public interface HelpHistoryRepository {
 
     HelpHistoryEntity parseJsonToEntity(Object json);
 
-    List<HelpCertificateEntity> getHelpCertificateList(Long memberId);
-
-    List<HelpCertificateEntity> getHelpCertificateEntityList(List<Object> objectList);
-
-    HelpCertificateEntity parseJsonToHelpCertificateEntity(Object json);
 }
