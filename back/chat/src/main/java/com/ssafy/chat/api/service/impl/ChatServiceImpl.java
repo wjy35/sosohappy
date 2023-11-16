9148b361583a40a09fc5eb101a396bc8a462d315
seokJKIM <tjrwn1247@naver.com>
2023-11-16 13:11:54 +0900

    :hammer: [BE] Refactor: socket response 리스트로 변경

--- back/chat/src/main/java/com/ssafy/chat/api/service/impl/ChatServiceImpl.java
+++ back/chat/src/main/java/com/ssafy/chat/api/service/impl/ChatServiceImpl.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,10 +31,12 @@ public class ChatServiceImpl implements ChatService {
                 .timestamp(chatPublish.getTimestamp())
                 .build();
 
+        List<ChatResponse> chatResponseList = new ArrayList<>();
+        chatResponseList.add(chatResponse);
 
         simpMessageSendingOperations.convertAndSend(
                 chatPublish.getChatRoomDetailDestination(),
-                objectSerializer.serialize(chatResponse)
+                objectSerializer.serialize(chatResponseList)
         );
 
     }
@@ -45,10 +48,11 @@ public class ChatServiceImpl implements ChatService {
                 .content(chatPublish.getContent())
                 .timestamp(chatPublish.getTimestamp())
                 .build();
-
+        List<ChatResponse> chatResponseList = new ArrayList<>();
+        chatResponseList.add(chatResponse);
         simpMessageSendingOperations.convertAndSend(
                 chatPublish.getChatRoomListDestination(),
-                objectSerializer.serialize(chatResponse)
+                objectSerializer.serialize(chatResponseList)
         );
     }
 
@@ -60,9 +64,12 @@ public class ChatServiceImpl implements ChatService {
                 .timestamp(chatPublish.getTimestamp())
                 .build();
 
+        List<ChatResponse> chatResponseList = new ArrayList<>();
+        chatResponseList.add(chatResponse);
+
         simpMessageSendingOperations.convertAndSend(
                 chatPublish.getChatRoomSelfDestination(),
-                objectSerializer.serialize(chatResponse)
+                objectSerializer.serialize(chatResponseList)
         );
     }
 
