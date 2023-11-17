c5822a5c49bfec616aae15b6e8bbdabed17c503c
youn <yyytae0@gmail.com>
2023-11-17 18:28:58 +0900

    :bug: [FE] Fix: set client clean to avoid error in client

--- front/src/hooks/useChatSocket.ts
+++ front/src/hooks/useChatSocket.ts
@@ -22,12 +22,14 @@ function useChatSocket() {
                 setConnected(true);
             },
             (error) => {
+                console.log('chatSocket error')
                 console.log(error)
             },
         );
     }
 
     function getList() {
+        if (!connected) return
         client.unsubscribe('list');
         client.unsubscribe('detail');
         setMsgList([]);
@@ -63,6 +65,7 @@ function useChatSocket() {
     }
 
     function getDetail(chatRoomId: number) {
+        if (!connected) return
         client.unsubscribe('list');
         client.unsubscribe('detail');
         setHelpChatList([]);
@@ -86,6 +89,7 @@ function useChatSocket() {
         client.disconnect(() => {
             console.log('chatSocket disconnect')
         });
+        setClient();
     }
 
     return {connect, connected, disConnect, getList, getDetail, helpChatList, msgList}
