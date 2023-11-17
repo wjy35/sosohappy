200f72ec4e94f04e508796011c1a2b60daffd1e5
youn <yyytae0@gmail.com>
2023-11-17 18:26:48 +0900

    :bug: [FE] Fix: fix keyboard avoid scrollview to keyboard avoid view

--- front/src/screens/Chat.tsx
+++ front/src/screens/Chat.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react"
-import {View, Text, TouchableOpacity, TextInput, ScrollView, Alert, FlatList} from "react-native";
+import {View, Text, TouchableOpacity, TextInput, ScrollView, Alert, FlatList, KeyboardAvoidingView} from "react-native";
 
 import CommonLayout from "@/components/CommonLayout";
 import MyChat from "@/components/MyChat";
@@ -97,7 +97,7 @@ const Chat = ({helpSocket, chatSocket}: propsType) => {
 
   return (
     <>
-    <KeyboardAwareScrollView>
+    {/*<KeyboardAwareScrollView>*/}
       <SafeAreaProvider>
         <SafeAreaView
             edges={["top", "right", "bottom", "left"]}
@@ -105,79 +105,81 @@ const Chat = ({helpSocket, chatSocket}: propsType) => {
         >
           <Header headerType={1} openSide={openSide}/>
           <SideMenu closeSide={closeSide} nowPage={'Chat'} isVisible={isVisible}/>
-          <View>
-            <View style={ChatStyle.heightWrap}>
-              <View style={ChatStyle.chatContentWrap}>
-                <FlatList
-                    data={[...chatSocket.msgList].reverse()}
-                    showsVerticalScrollIndicator={false}
-                    windowSize={5}
-                    inverted={true}
-                    initialNumToRender={10}
-                    renderItem={(item) => {
-                      if (item.item.memberId === userInfo.memberId){
-                        return <MyChat content={item.item.content}/>
-                      } else if (item.item.memberId === otherMemberId) {
-                        return <YourChat thumbnail={FishThumbnail} content={item.item.content}/>
-                      } else {
-                        return (
-                            <View style={{justifyContent: 'center', alignItems: 'center', marginVertical: 5}}>
-                              <Text>{item.item.content}</Text>
-                            </View>
-                        )
-                      }
-                    }}
-                    keyExtractor={(item) => String(item.timestamp)+String(item.memberId)+String(item.content)}
-                    onEndReachedThreshold={0.1}
-                    contentContainerStyle={{paddingHorizontal: 10}}
-                />
-              </View>
-              <View style={ChatStyle.agreeWrap}>
-                <SvgXml
-                    xml={people}
-                    width={45}
-                    height={45}
-                />
-                <Text style={ChatStyle.agreeInfoText}>매칭하기 버튼을 통해 상대방의 요청을 승인해주세요.</Text>
-                <TouchableOpacity activeOpacity={0.7}>
-                  <View style={ChatStyle.agreeButton}>
-                    <Text style={ChatStyle.agreeButtonText}>승인하기</Text>
+          <KeyboardAvoidingView style={{flex: 1}} behavior={'height'}>
+            <View style={{flex: 1}}>
+                <View style={ChatStyle.heightWrap}>
+                  <View style={ChatStyle.chatContentWrap}>
+                    <FlatList
+                        data={[...chatSocket.msgList].reverse()}
+                        showsVerticalScrollIndicator={false}
+                        windowSize={5}
+                        inverted={true}
+                        initialNumToRender={10}
+                        renderItem={(item) => {
+                          if (item.item.memberId === userInfo.memberId){
+                            return <MyChat content={item.item.content}/>
+                          } else if (item.item.memberId === route.params?.otherMemberId) {
+                            return <YourChat thumbnail={FishThumbnail} content={item.item.content}/>
+                          } else {
+                            return (
+                                <View style={{justifyContent: 'center', alignItems: 'center', marginVertical: 5}}>
+                                  <Text>{item.item.content}</Text>
+                                </View>
+                            )
+                          }
+                        }}
+                        keyExtractor={(item) => String(item.timestamp)+String(item.memberId)+String(item.content)}
+                        onEndReachedThreshold={0.1}
+                        contentContainerStyle={{paddingHorizontal: 10}}
+                    />
                   </View>
-                </TouchableOpacity>
-              </View>
+                  {/*<View style={ChatStyle.agreeWrap}>*/}
+                  {/*  <SvgXml*/}
+                  {/*      xml={people}*/}
+                  {/*      width={45}*/}
+                  {/*      height={45}*/}
+                  {/*  />*/}
+                  {/*  <Text style={ChatStyle.agreeInfoText}>매칭하기 버튼을 통해 상대방의 요청을 승인해주세요.</Text>*/}
+                  {/*  <TouchableOpacity activeOpacity={0.7}>*/}
+                  {/*    <View style={ChatStyle.agreeButton}>*/}
+                  {/*      <Text style={ChatStyle.agreeButtonText}>승인하기</Text>*/}
+                  {/*    </View>*/}
+                  {/*  </TouchableOpacity>*/}
+                  {/*</View>*/}
 
-              <View style={ChatStyle.controlWrap}>
-                <TouchableOpacity activeOpacity={0.7}>
-                  <View style={ChatStyle.exitFlexWrap}>
-                    <SvgXml
-                        xml={exit}
-                        width={20}
-                        height={20}
+                  <View style={ChatStyle.controlWrap}>
+                    <TouchableOpacity activeOpacity={0.7} onPress={()=>navigation.goBack()}>
+                      <View style={ChatStyle.exitFlexWrap}>
+                        <SvgXml
+                            xml={exit}
+                            width={20}
+                            height={20}
+                        />
+                        <Text style={ChatStyle.exitText}>채팅방 나가기</Text>
+                      </View>
+                    </TouchableOpacity>
+                    <TextInput
+                        placeholder="메시지 내용을 입력하세요."
+                        style={ChatStyle.chanMsgInput}
+                        onChangeText={(text) => setMsg(text)}
+                        value={msg}
                     />
-                    <Text style={ChatStyle.exitText}>채팅방 나가기</Text>
+                    <TouchableOpacity activeOpacity={0.7} onPress={sendMsg}>
+                      <View style={ChatStyle.sendButton}>
+                        <SvgXml
+                            xml={send2}
+                            width={20}
+                            height={20}
+                        />
+                      </View>
+                    </TouchableOpacity>
                   </View>
-                </TouchableOpacity>
-                <TextInput
-                    placeholder="메시지 내용을 입력하세요."
-                    style={ChatStyle.chanMsgInput}
-                    onChangeText={(text) => setMsg(text)}
-                    value={msg}
-                />
-                <TouchableOpacity activeOpacity={0.7} onPress={sendMsg}>
-                  <View style={ChatStyle.sendButton}>
-                    <SvgXml
-                        xml={send2}
-                        width={20}
-                        height={20}
-                    />
-                  </View>
-                </TouchableOpacity>
-              </View>
+                </View>
             </View>
-          </View>
+          </KeyboardAvoidingView>
         </SafeAreaView>
       </SafeAreaProvider>
-    </KeyboardAwareScrollView>
+    {/*</KeyboardAwareScrollView>*/}
     </>
   );
 };
