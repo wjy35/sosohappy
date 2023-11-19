73b1ff640f4e0e53d12cb6e4e94fff2854332cfb
윤태영 <rhkwpdusrn22@naver.com>
2023-11-19 14:36:24 +0900

    :heavy_plus_sign: [FE] Feat: sosomon name setting for dictionary
    TODO: fill index.ts in sosomon

--- front/src/components/MapLoading.tsx
+++ front/src/components/MapLoading.tsx
@@ -35,13 +35,13 @@ const MapLoading = ({socket}: propsType) => {
         const level = (userInfo.profileMonsterId % 10 === 0)?10:userInfo.profileMonsterId%10;
         // console.log(type, level)
         if (type === 1){
-            setSrc(type1[level-1])
+            setSrc(type1[level-1].src)
         } else if (type === 2){
-            setSrc(type2[level-1])
+            setSrc(type2[level-1].src)
         } else if (type === 3){
-            setSrc(type3[level-1])
+            setSrc(type3[level-1].src)
         } else {
-            setSrc(type4[0])
+            setSrc(type4[0].src)
         }
     }, []);
 
